f0e9d684dd9be4d869b9319c26e7ec01090c4724
Jacob577 <deltora.ericsson@gmail.com>
2021-06-24 22:05:23 +0200

    Testing to commit

--- Matrix.py
+++ Matrix.py
@@ -1,9 +1,22 @@
-import numpy
-
+import numpy as np
 
 class Matrix:
     def __init__(self):
-        self.rows = 6
-        self.columns = 7
-        self.matrix = numpy.zeros(self.rows,self.columns)
+        self.rows, self.columns = 6, 7
+        self.matrix = np.zeros([self.rows, self.columns])
 
+    def add_Chip(self, player, col):
+
+        try:
+            for i in range(self.rows):
+                if self.matrix[i][col] == 0:
+                    self.matrix[i][col] = player
+
+        except:
+            print("not okey for player {}".format(player))
+
+        finally:
+            print("Did not work")
+
+    def print_board(self):
+        print(self.matrix)
